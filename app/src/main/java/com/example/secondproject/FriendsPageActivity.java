29918b61381bf5d496eaa9757f97590b2d4a7514
baraksw <baraksw@users.noreply.github.com>
2020-01-26 14:57:43 +0200

    Updating the recycler view that it would get the data directly from the DB and remocing the background of the friends recycler view and my hums recycler view.

--- app/src/main/java/com/example/secondproject/FriendsPageActivity.java
+++ app/src/main/java/com/example/secondproject/FriendsPageActivity.java
@@ -1,46 +1,60 @@
 package com.example.secondproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.example.secondproject.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class FriendsPageActivity extends AppCompatActivity {
 
     private RecyclerView friend_recyclerView;
     private RecyclerView.LayoutManager friend_layoutManager;
     private FriendRecyclerAdapter friend_adapter;
-
-    private User user1 = new User();
-    private User user2 = new User();
-    private User user3 = new User();
-    private User user4 = new User();
-    private User user5 = new User();
-    private User user6 = new User();
-    private User user7 = new User();
-    private User user8 = new User();
-    private User user9 = new User();
-    private User user10 = new User();
-    private User user11 = new User();
-    private User user12 = new User();
-    private User user13 = new User();
-    private User my_friends_temp[] = {user1, user2, user3, user4, user5, user6, user7, user8, user9, user10, user11, user12, user13};
+    private ArrayList<User> my_friends_temp;
+    private DatabaseReference db_reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends_page);
+
         friend_recyclerView = findViewById(R.id.my_friends_recyclerView);
         friend_layoutManager = new GridLayoutManager(this, 1);
         friend_recyclerView.setHasFixedSize(true);
         friend_recyclerView.setLayoutManager(friend_layoutManager);
-        friend_adapter = new FriendRecyclerAdapter(my_friends_temp);
-        friend_recyclerView.setAdapter(friend_adapter);
 
+        db_reference = FirebaseDatabase.getInstance().getReference().child("users_db");
+        my_friends_temp = new ArrayList<>();
+        db_reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot dataSnapshot1: dataSnapshot.getChildren()){
+                    User user = dataSnapshot1.getValue(User.class);
+                    my_friends_temp.add(user);
+                }
+                friend_adapter = new FriendRecyclerAdapter(FriendsPageActivity.this, my_friends_temp);
+                friend_recyclerView.setAdapter(friend_adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(FriendsPageActivity.this, "Oops... something went wrong", Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 
     public void launchMyProfilePage(View view) {
