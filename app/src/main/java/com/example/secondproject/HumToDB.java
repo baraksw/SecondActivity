73dde891ec7f6b405769145644840427dd4659bc
baraksw <baraksw@users.noreply.github.com>
2020-03-01 14:03:41 +0200

    Fixing the uploading answer

--- app/src/main/java/com/example/secondproject/HumToDB.java
+++ app/src/main/java/com/example/secondproject/HumToDB.java
@@ -112,12 +112,16 @@ public interface HumToDB {
     }
 
     default boolean UploadAnswer(String answer, Hum hum) {
+        mDataBase.child("db2").child("hums_db").child(hum.getHum_id()).child("hum_answer").setValue(answer);
+        return true;
+    }
+        /*
         if (hum.getHum_answer() == "NULL") {
             mDataBase.child("db2").child("hums_db").child(hum.getHum_id()).child("hum_answer").setValue(answer);
             return true;
         } else
             return false;
-    }
+    }*/
 }
 
 
